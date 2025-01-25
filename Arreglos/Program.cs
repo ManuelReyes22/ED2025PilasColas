f165cb15fe39848e320c25d9631648d63878fd16
Mayra Tovar <mayra_ale99@hotmail.com>
2025-01-25 10:47:22 -0700

    Agregando solucion y proyecto de pilas con arreglos

---
+++ Arreglos/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilas pila = new Pilas(10);
+            pila.Agregar("a");
+            pila.Agregar("b");
+            pila.Agregar("c");
+            pila.Agregar("d");
+            pila.Agregar("e");
+            pila.Agregar("f");
+            pila.Agregar("g");
+            pila.Agregar("h");
+            pila.Agregar("i");
+            pila.Agregar("j");
+            pila.Agregar("k");
+            pila.Agregar("l");
+
+          
+            
+
+            string datos = pila.Imprimir();
+            Console.WriteLine(datos);
+
+            pila.Eliminar();
+            Console.WriteLine(pila.Imprimir());
+        }
+    }
+}
