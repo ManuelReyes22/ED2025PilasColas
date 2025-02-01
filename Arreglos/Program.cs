3805f9249a49f5792b0303829f357dcaa2e6068b
ManuelReyes22 <manuel202reyes@gmail.com>
2025-02-01 10:36:44 -0700

    terminando pilas y empesando con colas

--- Arreglos/Program.cs
+++ Arreglos/Program.cs
@@ -10,13 +10,13 @@
             pila.Agregar("c");
             pila.Agregar("d");
             pila.Agregar("e");
-            pila.Agregar("f");
-            pila.Agregar("g");
-            pila.Agregar("h");
-            pila.Agregar("i");
-            pila.Agregar("j");
-            pila.Agregar("k");
-            pila.Agregar("l");
+            //pila.Agregar("f");
+            //pila.Agregar("g");
+            //pila.Agregar("h");
+            //pila.Agregar("i");
+            //pila.Agregar("j");
+            //pila.Agregar("k");
+            //pila.Agregar("l");
 
           
             
